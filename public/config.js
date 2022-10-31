dcac23dd29cee8672c8d7aeeac084868c89f4669
MKRashed <mkrashed757@gmail.com>
2022-10-31 15:57:02 +0600

    about us

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://api.gcpsc.info';
+const BASE_URL = "https://api.gcpsc.info";
 
-const LOCAL_BASE_URL = 'http://192.168.88.189:8080';
+const LOCAL_BASE_URL = "http://192.168.20.242:8083";
